9a216d820c02370fff68ac9e36444db791a06daa
galanton <galantonj18@gmail.com>
2025-05-08 16:49:30 -0400

    front

---
+++ resources/views/compra.blade.php
@@ -0,0 +1,19 @@
+<?php
+include("./layouts/head.blade.php");
+
+?>
+
+<section id="compra" class="container container_compra">
+    <div class="container_compra">
+        <div class="cont_form">
+            <form action="#" method="POST" class="form_compra">
+                <input type="text" placeholder="nombre">
+                <input type="number" placeholder="cedula">
+                <input type="number" placeholder="referencia de pago">
+                <input type="number" placeholder="monto">
+                <input type="text" placeholder="metodo de pago">
+                <button class="button">enviar</button>
+            </form>
+        </div>
+    </div>
+</section>
